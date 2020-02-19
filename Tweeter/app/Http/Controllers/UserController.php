c5b584e938cde55dcb41effad6205f59ff45a3c1
Than Than Sint <masintlay@gmail.com>
2020-02-19 03:58:30 +0000

    updated final

--- Tweeter/app/Http/Controllers/UserController.php
+++ Tweeter/app/Http/Controllers/UserController.php
@@ -7,53 +7,6 @@ use Auth;
 
 class UserController extends Controller
 {
-    // public function homepage()
-    // {
-    //     return view('headerLayout');
-    // }
-
-    // public function checkLogin(Request $request){
-    //     $username = $request->username;
-    //     $password = $request->password;
-    //     $result = \App\User::where('username', $username)
-    //                     ->where ('password', $password)->get();
-    //     if (sizeOf($result)==1) {
-    //         return view('tweetFeed');
-    //     }else {
-    //         return view('loginFail');
-    //     }
-    // }
-    // public function userSignup(Request $request) {
-    //     $validatedData = $request->validate([
-    //         'username' => 'required|max:255',
-    //         // 'email'=>'required|email|unique:users,email',
-    //         'email'=>'email:rfc,dns',
-    //         'password'=>'required|max:255'
-    //     ]);
-
-    //     $username = $request->username;
-    //     $email = $request->email;
-    //     $password = $request->password;
-    //     $birthday = $request->month.'/'.$request->day.'/'.$request->year;
-    //     if ($request->female) {
-    //         $gender = "female";
-    //     } else if ($request->male){
-    //         $gender = "male";
-    //     } else {
-    //         $gender = "other";
-    //     }
-    //     $address = $request->city.' '.$request->country;
-
-    //     $user = new \App\User;
-    //     $user->username = $username;
-    //     $user->email = $email;
-    //     $user->password = $password;
-    //     $user->birthday = $birthday;
-    //     $user->gender = $gender;
-    //     $user->address = $address;
-    //     $user->save();
-    //     return view('headerLayout');
-    // }
     public function userProfile() {
         return view('userProfile');
     }
